44284d8a9fe057fe712927e7b86aff1d5677098c
Aleš Roubíček <ales@roubicek.name>
2022-12-10 10:32:34 +0100

    Bundling settings

--- display/vite.config.js
+++ display/vite.config.js
@@ -1,16 +1,21 @@
 import LitRefresh from "@dblechoc/plugin-lit-refresh";
-import {defineConfig} from "vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [LitRefresh()],
-    build: {
-        lib: {
-            entry: 'src/main.js',
-            formats: ['es']
-        },
-        rollupOptions: {
-            external: /^lit/
-        }
-    }
-})
+  root: "src",
+  plugins: [LitRefresh()],
+  build: {
+    target: "chrome100",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+      output: {
+        dir: "dist",
+      },
+      external: /^lit/,
+    },
+  },
+});
