71ca2e360404431c90b2e2d04ca78e1cd11d8cd6
Aleš Roubíček <ales@roubicek.name>
2025-09-16 16:00:19 +0200

    Reader uses CF Function and file name can be set via parameter

--- reader/main.js
+++ reader/main.js
@@ -1,12 +1,20 @@
-const text = await Deno.readTextFile("../XONE K2 - 4 decks (Live FX).xml");
-const resp = await fetch("https://serato-mapping-browser.deno.dev/", {
-  method: "POST",
-  headers: {
-    "Content-Type": "text/xml",
-    Accept: "application/json",
-  },
-  body: text,
-});
-const json = await resp.json();
+import { parseArgs } from "jsr:@std/cli/parse-args";
 
-console.log(JSON.stringify(json, null, 2));
+async function main({ file }) {
+  const text = await Deno.readTextFile(file);
+  const resp = await fetch("https://serato.rarousmusic.com/xml-to-json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/xml",
+      Accept: "application/json",
+    },
+    body: text,
+  });
+  const json = await resp.json();
+
+  console.log(JSON.stringify(json, null, 2));
+}
+
+await main(parseArgs(Deno.args))
+
+// deno --allow-read --allow-import --allow-net=serato.rarousmusic.com main.js --file=../XONEK2\ -\ 4\ decks\ -\ v4.0.xml | pbcopy
