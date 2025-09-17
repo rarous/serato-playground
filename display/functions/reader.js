1a514c35ee33cadb655544934f50db585ac7ed42
Aleš Roubíček <ales@roubicek.name>
2025-09-17 15:55:29 +0200

    Allow download XML export

--- display/functions/reader.js
+++ display/functions/reader.js
@@ -3,6 +3,7 @@
  * @returns {Promise<Response>}
  */
 export async function onRequestPost({ request }) {
+  const {searchParams} = new URL(request.url);
   const text = await request.text();
   const resp = await fetch("https://serato-mapping-browser.deno.dev/", {
     method: "POST",
@@ -12,6 +13,11 @@ export async function onRequestPost({ request }) {
     },
     body: text,
   });
+  if (searchParams.has("download")) {
+    const headers = new Headers(resp.headers);
+    headers.set("Content-Disposition", `attachment; filename="My Mapping.xml"`);
+    return new Response(resp.body, { status: resp.status, headers, statusText: resp.statusText })
+  }
   return resp;
 }
 
