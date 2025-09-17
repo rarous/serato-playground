a1b4983a1404004833e36330d62902864fe35b90
Aleš Roubíček <ales@roubicek.name>
2025-09-17 15:35:40 +0200

    Deduplicate XML elements, support JSON to XML via content type negotiation

--- reader/index.js
+++ reader/index.js
@@ -1,13 +1,34 @@
-import { parse } from "jsr:@libs/xml";
+import { parse, stringify } from "jsr:@libs/xml";
+import { fromJS, isKeyed } from "npm:immutable";
+
+function dedupe(xs) {
+  return fromJS(xs, (k,v,p) => isKeyed(v) ? v : v.toSet()).toJS()
+}
+
+async function readPayload(req) {
+  if (req.headers.get("Content-Type") === "application/json") {
+    return req.json();
+  }
+  const text = await req.text();
+  return dedupe(parse(text));
+}
 
 /**
  * @param {Request} req
  * @return {Promise<Response>}
  */
 export async function handler(req) {
-  const text = await req.text();
-  const xml = parse(text);
-  return Response.json(xml);
+  const xml = await readPayload(req);
+  if (req.headers.get("Accept") === "application/json") {
+    return Response.json(xml);
+  }
+  if (req.headers.get("Accept") === "text/xml") {
+    return new Response(stringify(xml), {
+      status: 200,
+      headers: { "Content-Type": "text/xml" },
+    });
+  }
+  return Response.error();
 }
 
 Deno.serve(handler);
