66f865a180af4e8cc1578dbd6ce3a10ff97cf575
Aleš Roubíček <ales@roubicek.name>
2025-09-17 16:32:19 +0200

    Send whole request to Deno Deploy

--- display/functions/reader.js
+++ display/functions/reader.js
@@ -4,15 +4,7 @@
  */
 export async function onRequestPost({ request }) {
   const { searchParams } = new URL(request.url);
-  const text = await request.text();
-  const resp = await fetch("https://serato-mapping-browser.deno.dev/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "text/xml",
-      Accept: "application/json",
-    },
-    body: text,
-  });
+  const resp = await fetch("https://serato-mapping-browser.deno.dev/", request);
   if (searchParams.has("download") && request.headers.get("Accept") === "text/xml" && resp.ok) {
     const headers = new Headers(resp.headers);
     headers.set("Content-Disposition", `attachment; filename="My Mapping.xml"`);
