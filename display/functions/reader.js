e9dc9fba57d7cf39182ca16c45e81dc08c22c09c
Aleš Roubíček <ales@roubicek.name>
2026-07-19 13:16:34 +0200

    Moved Deno handler to Cloudflare Pages Functions

--- display/functions/reader.js
+++ display/functions/reader.js
@@ -1,15 +1,46 @@
+import {parse, stringify} from "jsr:@libs/xml";
+import {fromJS, isKeyed} from "npm:immutable";
+
+function dedupe(xs) {
+  return fromJS(xs, (k, v, p) => isKeyed(v) ? v.toOrderedMap() : v.toOrderedSet()).toJS();
+}
+
+/**
+ *
+ * @param {Request} req
+ * @returns {Promise<*>}
+ */
+async function readPayload(req) {
+  if (req.headers.get("Content-Type") === "application/json") {
+    return req.json();
+  }
+  const text = await req.text();
+  return dedupe(parse(text));
+}
+
 /**
  * @param {EventContext} context
  * @returns {Promise<Response>}
  */
-export async function onRequestPost({ request }) {
-  const { searchParams } = new URL(request.url);
-  const resp = await fetch("https://serato-mapping-browser.deno.dev/", request);
-  if (searchParams.has("download") && request.headers.get("Accept") === "text/xml" && resp.ok) {
-    const headers = new Headers(resp.headers);
-    headers.set("Content-Disposition", `attachment; filename="My Mapping.xml"`);
-    return new Response(resp.body, { status: resp.status, headers, statusText: resp.statusText })
+export async function onRequestPost({request}) {
+  const {searchParams} = new URL(request.url);
+  const accept = request.headers.get("Accept");
+  const xml = await readPayload(request);
+  if (accept === "application/json") {
+    return Response.json(xml);
   }
-  return resp;
+  if (accept === "text/xml") {
+    let response = new Response(stringify(xml), {
+      status: 200,
+      headers: {"Content-Type": "text/xml"},
+    });
+    if (!searchParams.has("download")) {
+      return response;
+    }
+    const headers = new Headers(response.headers);
+    headers.set("Content-Disposition", `attachment; filename="My Mapping.xml"`);
+    return new Response(response.body, {status: response.status, headers, statusText: response.statusText});
+  }
+  return Response.error();
 }
 
